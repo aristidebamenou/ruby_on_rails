75c6f6ab03a3b9cd20ad6eab113c76f127a97a6c
aristidebamenou <aristide.bamenou@hotmail.com>
2022-03-19 13:47:00 +0100

    Node class fixing

--- linked_lists/main.rb
+++ linked_lists/main.rb
@@ -1,20 +1,17 @@
-class LinkedList
-  attr_accessor :head, :tail
-
-  def initialize(head=nil, tail=nil)
-    @head = head
-    @tail = tail
-  end
-
-end
-
+# Node class, containing a #value method and a link to the #next_node
 class Node
-  attr_accessor :value, :next_node
+  attr_reader :value
+  attr_accessor :next_node
 
-  def initialize(value=nil, next_node=nil)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
+end
+
+# LinkedList class, which will represent the full list
+class LinkedList
+  attr_accessor :head
 
 end
 
